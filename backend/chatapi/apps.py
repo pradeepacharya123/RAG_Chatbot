5aba9fc186d7ea1c8a0c8c83803e87603c0de92c
Pradeep <142654514+pradeepacharya123@users.noreply.github.com>
2025-08-20 20:11:50 +0530

    Update apps.py

--- backend/chatapi/apps.py
+++ backend/chatapi/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 class ChatapiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'chatapi'
+    name = 'backend.chatapi'
 
     def ready(self):
         import chatapi.signals
