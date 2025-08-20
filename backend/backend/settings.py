638bb5b637628cec3de73936f8d0589e5d76a291
Pradeep <142654514+pradeepacharya123@users.noreply.github.com>
2025-08-20 19:56:26 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
      'rest_framework',         # add this
     'corsheaders',            # add this
-    'chatapi',   
+    'backend.chatapi',   
 ]
 
 MIDDLEWARE = [
